46f27309b34fec80814c0a86d95907e711682eed
jiejie3593 <shaoyj3593@gmail.com>
2017-06-28 14:11:51 +0800

    delet duplications

--- mytest_script/t_bios_flash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-atiflash -p -f 0 /shareAll/yjshao/Linux_attach/BIOS/current/*
-sleep 10
-reboot
--- mytest_script/t_rm_log.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm *mtag*
-rm *yml*
-rm edc.log dpm.log stutter.log log.txt*
-rm report_*
-rm edc_*
